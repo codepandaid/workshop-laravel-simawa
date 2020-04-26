537918692229397d76ef9685f5ff0ee4523e4377
rafiqahsns <rafiqhsns@gmail.com>
2020-04-26 16:44:41 +0700

    clean code

--- resources/views/form-1.blade.php
+++ resources/views/form-1.blade.php
@@ -4,6 +4,7 @@
 @section('navbar_title','Form Input Mahasiswa')
 
 @section('content')
+
 <div style="text-align:center;">
     <span class="step active">1</span>
     <span class="step">2</span>
@@ -11,53 +12,52 @@
     <span class="step">4</span>
     <span class="step">5</span>
 </div>
+
 <div class="container">
     <div class="row justify-content-center">
-    <div class="col-lg-8 mt-5 mb-5">
-    <h4 class="text-center">
-        Data Individu
-    </h4>
-    <form>
-        <div class="form-group">
-            <label for="nama">Nama</label>
-            <input type="text" class="form-control" id="nama"  placeholder="Nama Lengkap">
+        <div class="col-lg-8 mt-5 mb-5">
+            <h4 class="text-center">
+                Data Individu
+            </h4>
+            <form>
+                <div class="form-group">
+                    <label for="nama">Nama</label>
+                    <input type="text" class="form-control" id="nama"  placeholder="Nama Lengkap">
+                </div>
+                <div class="form-group">
+                    <label for="nama">NIM</label>
+                    <input type="text" class="form-control" id="nama"  placeholder="NIM">
+                </div>
+                <div class="form-group">
+                    <label for="nama">Fakultas</label>
+                    <input type="text" class="form-control" id="nama"  placeholder="Fakultas">
+                </div>
+                <div class="form-group">
+                    <label for="nama">Departemen</label>
+                    <input type="text" class="form-control" id="nama"  placeholder="Departemen">
+                </div>
+                <div class="form-group">
+                    <label for="nama">Tahun Masuk</label>
+                    <input type="number" class="form-control" id="nama"  placeholder="Tahun Masuk">
+                </div>
+                <div class="form-group">
+                    <label for="nama">Departemen</label>
+                    <input type="text" class="form-control" id="nama"  placeholder="Departemen">
+                </div>
+                <div class="form-group">
+                    <label for="transkrip">Transkrip IPK Terakhir</label>
+                    <input type="file" class="form-control-file" id="transkrip">
+                </div>
+                <div class="form-group">
+                    <label for="spp">SPP Per Semester</label>
+                    <input type="number" class="form-control" id="spp"  placeholder="SPP Per Semester">
+                </div>
+                
+                <div class="mt-3" style="float:right">
+                    <button type="submit" class="btn btn-primary">Selanjutnya</button>
+                </div>
+            </form>
         </div>
-        <div class="form-group">
-            <label for="nama">NIM</label>
-            <input type="text" class="form-control" id="nama"  placeholder="NIM">
-        </div>
-        <div class="form-group">
-            <label for="nama">Fakultas</label>
-            <input type="text" class="form-control" id="nama"  placeholder="Fakultas">
-        </div>
-        <div class="form-group">
-            <label for="nama">Departemen</label>
-            <input type="text" class="form-control" id="nama"  placeholder="Departemen">
-        </div>
-        <div class="form-group">
-            <label for="nama">Tahun Masuk</label>
-            <input type="number" class="form-control" id="nama"  placeholder="Tahun Masuk">
-        </div>
-        <div class="form-group">
-            <label for="nama">Departemen</label>
-            <input type="text" class="form-control" id="nama"  placeholder="Departemen">
-        </div>
-        <div class="form-group">
-            <label for="transkrip">Transkrip IPK Terakhir</label>
-            <input type="file" class="form-control-file" id="transkrip">
-        </div>
-        <div class="form-group">
-            <label for="spp">SPP Per Semester</label>
-            <input type="number" class="form-control" id="spp"  placeholder="SPP Per Semester">
-        </div>
-
-        <div class="mt-3" style="float:right">
-        <button type="submit" class="btn btn-secondary">Sebelumnya</button>
-        <button type="submit" class="btn btn-primary">Selanjutnya</button>
-        </div>
-        
-    </form>
-</div>
     </div>
 </div>
 
--- resources/views/form-2.blade.php
+++ resources/views/form-2.blade.php
@@ -4,6 +4,7 @@
 @section('navbar_title','Form Input Mahasiswa')
 
 @section('content')
+
 <div style="text-align:center;">
     <span class="step finish">1</span>
     <span class="step active">2</span>
@@ -11,45 +12,45 @@
     <span class="step">4</span>
     <span class="step">5</span>
 </div>
+
 <div class="container">
     <div class="row justify-content-center">
-    <div class="col-lg-8 mt-5 mb-5">
-    <h4 class="text-center">
-        Informasi Keluarga
-    </h4>
-    <form>
-        <div class="form-group">
-            <label for="namaayah">Nama Ayah Kandung</label>
-            <input type="text" class="form-control" id="namaayah"  placeholder="Nama Ayah Kandung">
-        </div>
-        <div class="form-group">
-            <label for="namaibu">Nama Ibu Kandung</label>
-            <input type="text" class="form-control" id="namaibu"  placeholder="Nama Ibu Kandung">
-        </div>
-        <div class="form-group">
-            <label for="pekerjaanayah">Pekerjaan Ayah</label>
-            <input type="text" class="form-control" id="pekerjaanayah"  placeholder="Pekerjaan Ayah">
-        </div>
-        <div class="form-group">
-            <label for="pekerjaanibu">Pekerjaan Ibu</label>
-            <input type="text" class="form-control" id="pekerjaanibu"  placeholder="Pekerjaan Ibu">
-        </div>
-        <div class="form-group">
-            <label for="penghasilanayah">Penghasilan Ayah</label>
-            <input type="number" class="form-control" id="penghasilanayah"  placeholder="Penghasilan Ayah">
-        </div>
-        <div class="form-group">
-            <label for="penghasilanibu">Penghasilan Ibu</label>
-            <input type="number" class="form-control" id="penghasilanibu"  placeholder="Penghasilan Ibu">
-        </div>
+        <div class="col-lg-8 mt-5 mb-5">
+            <h4 class="text-center">
+                Informasi Keluarga
+            </h4>
+            <form>
+                <div class="form-group">
+                    <label for="namaayah">Nama Ayah Kandung</label>
+                    <input type="text" class="form-control" id="namaayah"  placeholder="Nama Ayah Kandung">
+                </div>
+                <div class="form-group">
+                    <label for="namaibu">Nama Ibu Kandung</label>
+                    <input type="text" class="form-control" id="namaibu"  placeholder="Nama Ibu Kandung">
+                </div>
+                <div class="form-group">
+                    <label for="pekerjaanayah">Pekerjaan Ayah</label>
+                    <input type="text" class="form-control" id="pekerjaanayah"  placeholder="Pekerjaan Ayah">
+                </div>
+                <div class="form-group">
+                    <label for="pekerjaanibu">Pekerjaan Ibu</label>
+                    <input type="text" class="form-control" id="pekerjaanibu"  placeholder="Pekerjaan Ibu">
+                </div>
+                <div class="form-group">
+                    <label for="penghasilanayah">Penghasilan Ayah</label>
+                    <input type="number" class="form-control" id="penghasilanayah"  placeholder="Penghasilan Ayah">
+                </div>
+                <div class="form-group">
+                    <label for="penghasilanibu">Penghasilan Ibu</label>
+                    <input type="number" class="form-control" id="penghasilanibu"  placeholder="Penghasilan Ibu">
+                </div>
 
-        <div class="mt-3" style="float:right">
-            <button type="submit" class="btn btn-secondary">Sebelumnya</button>
-            <button type="submit" class="btn btn-primary">Selanjutnya</button>
+                <div class="mt-3" style="float:right">
+                    <button type="submit" class="btn btn-secondary">Sebelumnya</button>
+                    <button type="submit" class="btn btn-primary">Selanjutnya</button>
+                </div>
+            </form>
         </div>
-        
-    </form>
-</div>
     </div>
 </div>
 
