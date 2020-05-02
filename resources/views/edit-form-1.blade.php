b096ae3450b738f8d649a18be58f68d5405ec3a3
rafiqahsns <rafiqhsns@gmail.com>
2020-05-03 00:06:20 +0700

    tambah edit form

---
+++ resources/views/edit-form-1.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.base')
+
+@section('title','Data Mahasiswa')
+@section('navbar_title','Edit Data Mahasiswa')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-8 mt-5 mb-5">
+            <h4 class="text-center">
+                Data Individu
+            </h4>
+            <form>
+                <div class="form-group text-center">
+                    <img id="profile" src="#" class="profile-image-input mt-3"/><br>
+                    <label class="custom-file-upload">
+                        <input id="inputProfile" type="file" accept="image/*">
+                        <i class="fas fa-plus mr-2"></i> Tambah gambar
+                    </label>
+                    <div class="help-block with-errors"></div>
+                </div>
+                <div class="form-group">
+                    <label for="nama">Nama</label>
+                    <input type="nama" class="form-control" id="nama" value=""  placeholder="Nama Lengkap">
+                    <div class="help-block with-errors"></div>
+                </div>
+                <div class="form-group">
+                    <label for="nim">NIM</label>
+                    <input type="text" class="form-control" id="nim" value=""  placeholder="NIM">
+                    <div class="help-block with-errors"></div>
+                </div>
+                <div class="form-group">
+                    <label for="fakultas">Fakultas</label>
+                    <input type="text" class="form-control" id="fakultas" value=""  placeholder="Fakultas">
+                    <div class="help-block with-errors"></div>
+                </div>
+                <div class="form-group">
+                    <label for="departemen">Departemen</label>
+                    <input type="text" class="form-control" id="departemen" value=""  placeholder="Departemen">
+                    <div class="help-block with-errors"></div>
+                </div>
+                <div class="form-group">
+                    <label for="tahunmasuk">Tahun Masuk</label>
+                    <input type="number" class="form-control" id="tahunmasuk" value=""  placeholder="Tahun Masuk">
+                    <div class="help-block with-errors"></div>
+                </div>
+                <div class="form-group">
+                    <label for="transkrip">Transkrip IPK Terakhir</label>
+                    <input type="file" class="form-control-file" id="transkrip">
+                    <div class="help-block with-errors"></div>
+                </div>
+                <div class="form-group">
+                    <label for="spp">SPP Per Semester</label>
+                    <input type="number" class="form-control" id="spp" value="" placeholder="SPP Per Semester">
+                    <div class="help-block with-errors"></div>
+                </div>
+                
+                <div class="mt-3" style="float:right">
+                    <button type="submit" class="btn btn-primary">Simpan</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+
+
+@endsection
